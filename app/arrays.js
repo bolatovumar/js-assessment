0f82313adebfd535990aa5e6faa10c9f4d4e1034
bolatovumar <bolatovumar@gmail.com>
2015-08-19 19:10:16 -0700

    Added comments

--- app/arrays.js
+++ app/arrays.js
@@ -1,23 +1,26 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
-
+  // determine the location of an item in an array
   indexOf : function(arr, item) {
     return arr.indexOf(item);
   },
 
+  // add the values of an array
   sum : function(arr) {
     return arr.reduce(function(curr, next) {
       return curr + next;
     });
   },
 
+  // remove all instances of a value from an array
   remove : function(arr, item) {
     return arr.filter(function(el) {
       return el !== item;
     });
   },
 
+  // remove all instances of a value from an array, returning the original array
   removeWithoutCopy : function(arr, item) {
     for (var i = arr.length - 1; i >= 0; i--) {
       if (arr[i] === item) {
@@ -28,40 +31,47 @@ exports.arraysAnswers = {
     return arr;
   },
 
+  // add an item to the end of an array
   append : function(arr, item) {
     arr.push(item);
 
     return arr;
   },
 
+  // remove the last item of an array
   truncate : function(arr) {
     arr.pop();
 
     return  arr;
   },
 
+  // add an item to the beginning of an array
   prepend : function(arr, item) {
     arr.unshift(item);
 
     return arr;
   },
 
+  // remove the first item of an array
   curtail : function(arr) {
     arr.shift();
 
     return  arr;
   },
 
+  // join together two arrays
   concat : function(arr1, arr2) {
     return arr1.concat(arr2);
   },
 
+  // add an item anywhere in an array
   insert : function(arr, item, index) {
     arr.splice(index, 0, item);
 
     return arr;
   },
 
+  // count the occurences of an item in an array
   count : function(arr, item) {
     var counter = 0;
     arr.forEach(function(el) {
@@ -71,6 +81,7 @@ exports.arraysAnswers = {
     return counter;
   },
 
+  // find duplicates in an array
   duplicates : function(arr) {
     var duplicates = [];
     for (var i = 0; i < arr.length; i++) {
@@ -85,12 +96,14 @@ exports.arraysAnswers = {
     return duplicates;
   },
 
+  // square each number in an array
   square : function(arr) {
     return arr.map(function(el) {
       return el*el;
     });
   },
 
+  // find all occurrences of an item in an array
   findAllOccurrences : function(arr, target) {
     var instances = [];
     arr.forEach(function(el, i) {
