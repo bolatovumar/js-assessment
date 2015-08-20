0ac2ff69e80f5a860ed88eaa277457d379a172d5
bolatovumar <bolatovumar@gmail.com>
2015-08-19 20:31:20 -0700

    Added answers for logicalOperators.js

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -1,11 +1,13 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.logicalOperatorsAnswers = {
+  // should be able to work with logical or
   or : function(a, b) {
-
+    return a || b;
   },
 
+  // should be able to work with logical and
   and : function(a, b) {
-
+    return a && b;
   }
 };
