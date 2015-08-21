977162707e0cc39055c57b051f70a167357e9538
bolatovumar <bolatovumar@gmail.com>
2015-08-20 19:15:55 -0700

    Added answer for modules.js

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,15 @@
 exports = (typeof window === 'undefined') ? global : window;
 
+// create a function that returns a module
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
-
+    var module = {
+        name: str2,
+        greeting: str1,
+        sayIt: function() {
+            return this.greeting + ", " + this.name;
+        }
+    }
+    return module;
   }
 };
