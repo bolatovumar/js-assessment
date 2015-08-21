8a9dd7f004c8fb335090fbf9e9d9949e78ab70c5
bolatovumar <bolatovumar@gmail.com>
2015-08-20 23:37:22 -0700

    Added answers for objects.js

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,23 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
+  // alter the context in which a method runs
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
+  // alter multiple objects at once
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
+  // iterate over an object's "own" properties
+  // using underscore function since it's included in the repo
   iterate : function(obj) {
-
+    var props = [];
+    _.mapObject(obj, function(val, key) {
+      props.push(key + ": " + val);
+    });
+    return props;
   }
 };
