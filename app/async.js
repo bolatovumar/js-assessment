dbe08be238ef3ca102ddcecfaeaa5d4701e48f51
bolatovumar <bolatovumar@gmail.com>
2015-08-20 23:51:16 -0700

    Added answers for async.js

--- app/async.js
+++ app/async.js
@@ -2,10 +2,17 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return $.Deferred().resolve(value);
   },
 
   manipulateRemoteData : function(url) {
-
+    return $.get(url).then(
+        function(data) {
+            return data.people.map(function(el) {
+              return el.name;
+            })
+            .sort();
+        }
+    );
   }
 };
